d59be66a2afa20066dd66301a75386c7f44156e0
Tim Haakman <tim.haakman@gmail.com>
2025-12-01 11:11:05 +0100

    2.1

---
+++ Assets/Scripts/ShootBall.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+
+public class NewMonoBehaviourScript : MonoBehaviour
+{
+    public float ShootForce = 500f;
+    public Vector3 Direction = new Vector3(0f,1f,0f);
+    private Rigidbody2D rb;
+    
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    
+    void Update()
+    {
+         if(Input.GetKeyDown(KeyCode.Space))
+        {
+        // Geeft een kracht in de opgegeven richting * sterkte
+        rb.AddForce(Direction * ShootForce);
+        
+        }
+    }
+}
